56c618dcfe38021f43f367443144e76acf98f27b
Golap12 <golaphasan379@gmail.com>
2024-04-28 01:41:12 +0600

    Add item post, AddItem get completed, Get home data completed,

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const cors = require('cors');
+const homeData = require('./homeData.json')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0cyoac0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const addedItemCollection = client.db("artCraftDB").collection('addedItem');
+
+
+    // Get myItems data
+    app.get('/myItems/:email', async (req, res) => {
+      const result = await addedItemCollection.find({email:req.params.email}).toArray();
+      res.send(result)
+    })
+
+
+
+    // Get AddItem data
+    app.get('/addeditems', async (req, res) => {
+        const cursor = addedItemCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
+    
+    // get home data
+    app.get('/homeData', (req, res) => {
+        res.send(homeData)
+    })
+
+
+     
+    app.post('/addeditems', async (req, res) => {
+        const newAddItem = req.body;
+        const result = await addedItemCollection.insertOne(newAddItem);
+        res.send(result);
+    })
+
+
+    
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Assignment 10 server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Assignment 10 server is running on port ${port}`);
+})
